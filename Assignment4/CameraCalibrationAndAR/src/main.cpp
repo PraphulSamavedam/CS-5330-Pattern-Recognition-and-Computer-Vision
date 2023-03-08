a085255ef716d4d37311e0402db0ec1a62004bfa
PraphulSamavedam <praphulsamavedam@gmail.com>
2023-03-08 15:02:32 -0500

    calibrate camera is working, reprojection error failure, Open new GitHub issue.

--- Assignment4/CameraCalibrationAndAR/src/main.cpp
+++ Assignment4/CameraCalibrationAndAR/src/main.cpp
@@ -32,6 +32,7 @@ int main(int argc, char argv[]) {
 	std::vector<cv::Vec3f> last_points_set;
 	std::vector<std::vector<cv::Vec3f>> points_list;
 
+
 	bool last_successful_capture = false;
 	while (true) {
 		*capture >> frame;
@@ -42,7 +43,7 @@ int main(int argc, char argv[]) {
 		}
 		std::vector<cv::Point2f> corners_set;
 		bool status = detectAndExtractChessBoardCorners(frame, corners_set);
-		printf("Status of chess board capture in video loop: %d\n", status);
+		// printf("Status of chess board capture in video loop: %d\n", status);
 		cv::namedWindow("Image", cv::WINDOW_GUI_EXPANDED);
 		cv::imshow("Image", frame);
 		char key = cv::waitKey(10);
@@ -88,6 +89,91 @@ int main(int argc, char argv[]) {
 					continue;
 				}
 			}
+		}
+		else if (key == 'c')
+		{
+
+			// Image Size is refs
+			cv::Mat cameraMatrix = cv::Mat::zeros(3, 3, CV_64FC1);
+			cameraMatrix.at<float>(0,0) = 1;
+			cameraMatrix.at<float>(1, 1) = 1;
+			cameraMatrix.at<float>(2, 2) = 1;
+			cameraMatrix.at<float>(0, 2) = frame.cols / 2;
+			cameraMatrix.at<float>(1, 2) = frame.rows/2;
+			std::vector<float> distortionCoefficients;
+			for (int i = 0; i < 8; i++)
+			{
+				distortionCoefficients.push_back(0.0);
+			}
+			std::vector<cv::Mat> rVecs;
+			std::vector<cv::Mat> tVecs;
+
+			printf("Pre Camera Matrix\n");
+			for (int i=0;i<3;i++ )
+			{
+				for (int j = 0; j < 3; j++)
+				{
+					printf("[%d,%d]: %.02f", i, j, cameraMatrix.at<double>(i, j));
+				}
+				printf("\n");
+			}
+
+			printf("Pre distortion coefficients are :\n");
+			for (auto dist : distortionCoefficients)
+			{
+				std::cout << dist << std::endl;
+			}
+			printf("Using %zd points for calibration.\n", corners_list.size());
+			cv::calibrateCamera(points_list, corners_list, refs, cameraMatrix, 
+				distortionCoefficients, rVecs, tVecs, cv::CALIB_FIX_ASPECT_RATIO);
+			printf("Calibrated the camera.\n");
+			printf("Updated distortion coefficients are :\n");
+			for (auto dist:distortionCoefficients) 
+			{
+				std::cout << dist << std::endl;
+			}
+			printf("Updated Camera Matrix\n");
+			for (int i = 0; i < 3; i++)
+			{
+				for (int j = 0; j < 3; j++)
+				{
+					printf("[%d,%d]: %.06f ", i, j, cameraMatrix.at<double>(i, j));
+				}
+				printf("\n");
+			}
+
+			//Calculating re-projection error ToDo Error Calculation failure
+			float reprojection_error = 0.0;
+			for (int i = 0; i < points_list.size(); i++)
+			{
+				std::vector<cv::Point3f> original_points_set;
+				for (int j = 0; j < points_list[i].size(); j++)
+				{
+					original_points_set.push_back(cv::Point3f(points_list[i][j][0], points_list[i][j][1], points_list[i][j][2]));
+				}
+				std::vector<cv::Point2f> projected_corners_set;
+				cv::projectPoints(original_points_set, rVecs, tVecs, cameraMatrix, distortionCoefficients, projected_corners_set);
+
+				//debug
+				printf("Projected Corners set size:%zd", projected_corners_set.size());
+				for (auto vr:projected_corners_set)
+				{
+					std::cout << vr << std::endl;
+				}
+				printf("debug step pass");
+
+				//Calculate the error for this projected points set
+				for (int j = 0; j < corners_set.size(); j++)
+				{
+					double dx = (corners_list[i][j].x - projected_corners_set[j].x);
+					double dy = (corners_list[i][j].y - projected_corners_set[j].y);
+					reprojection_error += ((dx * dx) + (dy * dy));
+				}
+
+			}
+			printf("Reprojection Error: %.06f ", reprojection_error);
+			printf("Closing the function");
+			break;
 		}
 	}
 	return 0;
