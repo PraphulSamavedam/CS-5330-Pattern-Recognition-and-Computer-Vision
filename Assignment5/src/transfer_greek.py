816ddddc5fdefab6b02b59911821479b57c84c03
PraphulSamavedam <praphulsamavedam@gmail.com>
2023-04-05 02:24:53 -0400

    Stable transfer learned greek letters

--- Assignment5/src/transfer_greek.py
+++ Assignment5/src/transfer_greek.py
@@ -41,44 +41,23 @@ def freeze_layers_and_modify_last_layer(model: nn.Module, output_features: int):
                                                bias = True)
     return model
 
-
-def main():
-    """This is the function which runs when run as a standalone script.
-    Returns
-    0 if the script exits successfully."""
-
-    epochs = 25
-    learning_rate = 0.03
-    momentum = 0.1
-    log_interval = 5
-    batch_size = 5
-
-    # Load the model from file
-    model = load_model(model_path="models/final_model.pth")
-    # Modify the model to freeze all layers & modify last FC layer to fit
-    model = freeze_layers_and_modify_last_layer(model=model, output_features=3)
-
-    greek_train = torch.utils.data.DataLoader(
-        torchvision.datasets.ImageFolder(root="data/greek_train",
-                                         transform=torchvision.transforms.Compose([
-                                            torchvision.transforms.ToTensor(),
-                                            GreekTransform(),
-                                            torchvision.transforms.Normalize((0.1307,),
-                                                                             (0.3801,),)
-                                         ])), batch_size = batch_size, shuffle=True )
-
+def train_and_plot_accuracies(model: nn.Module, epochs: int, optimizer: torch.optim,
+                              train_data_loader: torch.utils.data.DataLoader, log_interval:int,
+                              batch_size: int):
+    """This function trains the network for the train data loader and plots 
+    the accuracies vs epochs and 
+    training losses vs training examples seen """
     train_losses = []
     train_indices = []
     accuracies = []
-    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)
     for epoch in range(1, epochs+1, 1):
-        losses, indices = train_network(model= model, train_data_loader= greek_train,
+        losses, indices = train_network(model= model, train_data_loader= train_data_loader,
                     optimizer=optimizer, log_interval=log_interval,
                     epoch=epoch, batch_size=batch_size,
                     model_path="models/model_greek.pth", optim_path="models/optim_greek.pth")
         train_losses.extend(losses)
         train_indices.extend(indices)
-        _, accuracy = test_network(model=model, test_data_loader=greek_train)
+        _, accuracy = test_network(model=model, test_data_loader=train_data_loader)
         accuracies.append(accuracy)
     plt.figure(figsize=(10, 10))
     plt.plot(train_indices, train_losses, c='g', label="Training loss")
@@ -97,6 +76,39 @@ def main():
     plt.legend()
     plt.savefig("results/transfer_learning_accuracies.png")
     plt.show()
+
+
+def main():
+    """This is the function which runs when run as a standalone script.
+    Returns
+    0 if the script exits successfully."""
+
+    epochs = 25
+    learning_rate = 0.03
+    momentum = 0.1
+    log_interval = 5
+    batch_size = 5
+
+    # Load the model from file
+    model = load_model(model_path="models/final_model.pth")
+    # Modify the model to freeze all layers & modify last FC layer to fit
+    model = freeze_layers_and_modify_last_layer(model=model, output_features=3)
+
+    # Train dataset provided 
+    greek_train = torch.utils.data.DataLoader(
+        torchvision.datasets.ImageFolder(root="data/greek_train",
+                                         transform=torchvision.transforms.Compose([
+                                            torchvision.transforms.ToTensor(),
+                                            GreekTransform(),
+                                            torchvision.transforms.Normalize((0.1307,),
+                                                                             (0.3801,),)
+                                         ])), batch_size = batch_size, shuffle=True )
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)
+    # Train and plot the accuracy based on the 
+    train_and_plot_accuracies(model=model, epochs=epochs, optimizer=optimizer, 
+                              train_data_loader=greek_train, log_interval=log_interval, 
+                              batch_size=batch_size)
+
 
 if __name__ == "__main__":
     main()
